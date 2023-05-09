02516499f60ea314925517ed8bf0c5b1bf776c86
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-09 07:49:07 -0500

    re-write correct code for card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -1,20 +1,23 @@
 class Card
-    attr_accessor :question, :answer, :category 
-    def initialize 
-        @question = "What is the capital of Alaska?"
-        @answer = "Juneau"
-        @category = :Geography 
+    attr_reader :question, :answer, :category 
+    def initialize(question = "What is the capital of Alaska?", answer = "Juneau", category = :Geography)
+        @question = question
+        @answer = answer
+        @category = category
     end 
-    def be_instance_of(Card)
-       
-    end 
-    def question
-        @question
-    end 
-    def answer
-        @answer
-    end 
-    def category
-        @category
-    end 
+    # # def be_instance_of(Card)
+    # #    card = Card.new
+    # # end 
+    # def question
+    #     # @question
+    #     "What is the capital of Alaska?"
+    # end 
+    # def answer
+    #     # @answer
+    #     "Juneau"
+    # end 
+    # def category
+    #     # @category
+    #     :Geography
+    # end 
 end 
