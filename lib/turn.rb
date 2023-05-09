4ac5ab1d97e224d8605644e5669f77806822bb5e
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-09 09:15:17 -0500

    add initial code to turn lib file

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+class Turn 
+    attr_reader :string, :card 
+    def initialize(string, card)
+        @string = string 
+        @card = card
+    end 
+    def guess
+        @string
+    end 
+    def correct?
+        if turn.guess == card.answer 
+            true
+        else 
+            false
+        end 
+    end 
+    def feedback
+        if turn.correct? == true
+            "Correct!"
+        else 
+            "Inncorrect."
+        end 
+    end 
+end 
