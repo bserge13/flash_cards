325fd906b5b0c6c4aaa9b33916ef12b82c3cb3d7
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-09 14:14:56 -0500

    add two new cards

--- lib/card.rb
+++ lib/card.rb
@@ -1,25 +1,14 @@
 class Card
-    attr_reader :question, :answer, :category 
+    attr_reader :question, 
+                :answer, 
+                :category 
+
     def initialize(question = "What is the capital of Alaska?", answer = "Juneau", category = :Geography)
+        
         @question = question
         @answer = answer
         @category = category
     end 
-    # # def be_instance_of(Card)
-    # #    card = Card.new
-    # # end 
-    # def question
-    #     # @question
-    #     "What is the capital of Alaska?"
-    # end 
-    # def answer
-    #     # @answer
-    #     "Juneau"
-    # end 
-    # def category
-    #     # @category
-    #     :Geography
-    # end 
 end 
 
 # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
