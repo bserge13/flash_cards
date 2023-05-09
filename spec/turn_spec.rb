3d763d5f9ef20c27f1a37e4ce0feb301b17ef1d1
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-09 14:17:18 -0500

    add to the exists test

---
+++ spec/turn_spec.rb
@@ -0,0 +1,15 @@
+require './lib/turn'
+require './lib/card'
+
+Rspec.describe Turn do 
+    it 'exists' do
+        # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
+        # card_3 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+        # turn_1 = Turn.new("Juneau", card_1)
+        # turn_2 = Turn.new("Abraham Lincoln", card_2)
+        # turn_3 = Turn.new("Mercury", card_3)
+
+    end 
+end 
