90f001351411da5ed602b53379f3ed477194af3b
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-10 06:39:31 -0500

    revert card specfile back to original state

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,42 +2,22 @@ require './lib/card'
 
 RSpec.describe Card do
   it 'exists' do
-    # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
-    # card_3 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-    # expect(card_1).to be_instance_of(Card)
-    # expect(card_2).to be_instance_of(Card)
-    # expect(card_3).to be_instance_of(Card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(card).to be_instance_of(Card)
   end
 
   it 'has a question' do
-    # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
-    # card_3 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-    # expect(card_1.question).to eq("What is the capital of Alaska?")
-    # expect(card_2.question).to eq("Who was the 16th President of the US?")
-    # expect(card_3.question).to eq("Which planet is closest to the sun?")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(card.question).to eq("What is the capital of Alaska?")
   end
 
   it 'has an answer' do
-    # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    # card_2 = Card.new("Who was the 16th President of the US?")
-    # card_3 = Card.new("Which planet is closest to the sun?")
-
-    # expect(card_1.answer).to eq("Juneau")
-    # expect(card_2.answer).to eq("Abraham Lincoln")
-    # expect(card_3.answer).to eq("Mercury")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(card.answer).to eq("Juneau")
   end
 
   it 'has a category' do
-    # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
-    # card_3 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    
-    # expect(card_1.category).to eq(:Geography)
-    # expect(card_2.category).to eq(:History)
-    # expect(card_3.category).to eq(:STEM)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(card.category).to eq(:Geography)
   end
 end
