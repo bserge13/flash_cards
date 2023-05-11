88afa6cc9cac1aa9efc5ef92d68b9e968916ed88
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-10 20:30:56 -0500

    push final turn spec code

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,15 +1,37 @@
 require './lib/turn'
 require './lib/card'
 
-Rspec.describe Turn do 
+RSpec.describe Turn do 
     it 'exists' do
-        # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
-        # card_3 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-        # turn_1 = Turn.new("Juneau", card_1)
-        # turn_2 = Turn.new("Abraham Lincoln", card_2)
-        # turn_3 = Turn.new("Mercury", card_3)
-
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn_1 = Turn.new("Juneau", card_1)
+        expect(turn_1).to be_instance_of(Turn)
     end 
+
+    it 'has a guess' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
+        # card_3 Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+       
+        turn_1 = Turn.new("Juneau", card_1)
+        expect(turn_1.guess).to eq(card_1.answer) 
+    end
+
+    it 'gets feedback on guess' do 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
+        # card_3 Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        
+        turn_1 = Turn.new("Juneau", card_1)
+        expect(turn_1.feedback).to eq("Correct!")
+    end
+
+    it 'has an incorrect guess' do 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        # card_2 = Card.new("Who was the 16th President of the US?", "Abraham Lincoln", :History)
+        # card_3 Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        
+        turn_1 = Turn.new("Seattle", card_1)
+        expect(turn_1.feedback).to eq("Inncorrect.")
+    end
 end 
